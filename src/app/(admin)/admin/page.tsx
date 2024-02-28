e68dff6c750e46d845265430efef13c4b62f1aa8
langgamrhapsody <langgam.rhapsody@gmail.com>
2024-02-29 01:28:40 +0800

    fix 3

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import React, { FC, useEffect } from "react";
 import AdminTable from "./adminTable";
 import { cookies } from "next/headers";
@@ -6,19 +8,6 @@ import {
   CustomerListData,
   adminServiceController,
 } from "@/app/service/adminServiceController";
-
-const dummyData = [
-  {
-    id: "22739f0d-bb7a-4032-97ca-b8b292e5541a",
-    customerId: "e783065d-11dc-4bb5-8086-d5ed7a8173f7",
-    status: "requested",
-  },
-  {
-    id: "ca8d0b7c-62ad-465e-8a3e-5b9f7b3fe14e",
-    customerId: "8be1b2b5-c173-46fb-bd60-85c349ecf995",
-    status: "accepted",
-  },
-];
 
 const Page = async () => {
   const getRequestList = async () => {
--- src/app/(site)/user/page.tsx
+++ src/app/(site)/user/page.tsx
@@ -1,4 +1,4 @@
-// "use client";
+export const dynamic = "force-dynamic";
 import React, { useEffect } from "react";
 import {
   Vehicle,
